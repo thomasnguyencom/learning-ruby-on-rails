c65b93b568140bd9fc38be5aeb48c6a76d3d826f
thomasnguyencom <contact@thomasnguyen.com>
2013-01-10 21:57:48 -0500

    Add validation to the Post model

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
+  
+  validates :name, :prescence => true
+  validates :title, :prescence => true,
+    :length => {minimum => 5}
 end
