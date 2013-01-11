b3f1a52c422b038afa28c6de23b2668670672ce5
thomasnguyencom <contact@thomasnguyen.com>
2013-01-10 23:03:42 -0500

    Fix spelling - :presence

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
   
-  validates :name, :prescence => true
-  validates :title, :prescence => true,
-    :length => {minimum => 5}
+  validates :name, :presence => true
+  validates :title, :presence => true,
+    :length => { :minimum => 5}
 end
